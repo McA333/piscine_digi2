68092cb35f987615b8a698ea35c98371f252b371
Marie Caron <marie.caron@epitech.eu>
2025-09-02 11:50:32 +0200

    feat: conditions for firt num = seconde num, first num > seconde num and first num < seconde num.

--- partie1.py
+++ partie1.py
@@ -2,4 +2,11 @@ a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 result = a + b
 
+if (a == b) :
+    print("Les deux nombres sont égaux")
+if (a > b) :
+    print("Le premier nombre est plus grand que le second")
+if (a < b) :
+    print("Le premier nombre est plus petit que le second")
+
 print (result)
