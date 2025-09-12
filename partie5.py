0e14e122a6575f5a2a13e4951c4bfbf17c07a716
Marie Caron <marie.caron@epitech.eu>
2025-09-12 15:07:55 +0200

    feat: creation print line.

--- partie5.py
+++ partie5.py
@@ -3,6 +3,6 @@ import sys
 def list_discovery() -> list:
 
     list = [sys.argv[5], sys.argv[4], sys.argv[3], sys.argv[2], sys.argv[6]]
-    print(list)
+    print("Numbers has {x} elements and the sum of them all is {somme}.\n",list)
 
 list_discovery()
